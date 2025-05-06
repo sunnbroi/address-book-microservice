3f15d0ceea6177cfaacd699e6314176fa42ecf24
Alex <kochoryan2000.jh@gmail.com>
2025-05-06 18:44:43 +0300

    удалил старую версию

--- generate_token.php
+++
@@ -1,6 +0,0 @@
-<?php
-$body = '{"address_book_id":"a02e1de8-2f1b-4e11-b456-94e3b6072759","text":"Привет, пользователь!"}';
-$secret = 'supersecret';
-
-$token = hash_hmac('sha256', $body, $secret);
-echo "TOKEN: " . $token . PHP_EOL;
